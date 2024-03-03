27037a1a046a0de9cccd190d2cdf4a5fb3f3aa15
jiji-thecat <jijithekawaiicat@gmail.com>
2024-03-03 16:34:34 +0900

    add: background color to fulfill entire window

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,5 +1,5 @@
 const canvas = <HTMLCanvasElement> document.getElementById('canvas'); // cast
 const ctx = <CanvasRenderingContext2D> canvas.getContext('2d');
 
-ctx.fillStyle = 'pink';
-ctx.fillRect(10, 10, 150, 100);
+ctx.fillStyle = 'skyblue';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
