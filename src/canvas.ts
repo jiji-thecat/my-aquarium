97cd56a08017711e95d26f41542bd765b97ea163
jiji-thecat <jijithekawaiicat@gmail.com>
2024-03-10 23:21:18 +0900

    resize

--- src/canvas.ts
+++ src/canvas.ts
@@ -3,3 +3,28 @@ const ctx = <CanvasRenderingContext2D> canvas.getContext('2d');
 
 ctx.fillStyle = 'skyblue';
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+var img = new Image();
+img.src = "../assets/tuna.png";
+
+var maxWidth = 80; // Define the maximum width of the image
+  var maxHeight = 80; // Define the maximum height of the image
+  var width = img.width;
+  var height = img.height;
+
+  // Calculate the new dimensions, maintaining the aspect ratio
+  if (width > height) {
+    if (width > maxWidth) {
+      height *= maxWidth / width;
+      width = maxWidth;
+    }
+  } else {
+    if (height > maxHeight) {
+      width *= maxHeight / height;
+      height = maxHeight;
+    }
+  }
+  
+img.onload = () => {
+    ctx.drawImage(img, 0, 0, width, height);
+}
