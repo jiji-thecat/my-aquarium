ee1ace5a9b5cd514e800cc1eff867f80a1603d04
jiji-thecat <jijithekawaiicat@gmail.com>
2024-04-07 23:01:21 +0900

    add slider

---
+++ bin/index.js
@@ -0,0 +1,50 @@
+import Fish from './fish';
+class Main {
+    constructor() {
+        this._fps = 30;
+        this._fileNumber = 11;
+        this._fish = [];
+        this.render = () => {
+            this._ctx.globalCompositeOperation = 'destination-over';
+            const animation = () => {
+                setTimeout(() => {
+                    this._ctx.clearRect(0, 0, this._canvas.width, this._canvas.height); // clear canvas
+                    this._fish.forEach((f) => {
+                        const random = f.getRandomPosition();
+                        this._ctx.translate(0, f.random);
+                        if (f.isRight && f.x < this._canvas.width) {
+                            const rad = ((f.count * Math.PI) / 180) * random;
+                            f.x = rad * random;
+                            f.y = Math.sin(rad) * random;
+                            this._ctx.translate(f.x, f.y);
+                            this._ctx.scale(-1, 1);
+                            f.addCount({ isTrue: true });
+                        }
+                        else if (f.x > 0) {
+                            f.addCount({ isTrue: false });
+                            f.isRight = false;
+                            const rad = (f.count * random * Math.PI) / 180;
+                            f.x = rad * random;
+                            f.y = Math.sin(rad) * random;
+                            this._ctx.translate(f.x, f.y);
+                            this._ctx.scale(1, 1);
+                        }
+                        else {
+                            f.isRight = true;
+                        }
+                        this._ctx.drawImage(f.image, 0, 0, f.width, f.height);
+                        this._ctx.setTransform(1, 0, 0, 1, 0, 0);
+                    });
+                    requestAnimationFrame(animation);
+                }, 1000 / this._fps);
+            };
+        };
+        this._canvas = document.getElementById('canvas'); // cast
+        this._ctx = this._canvas.getContext('2d');
+        for (let i = 1; i <= this._fileNumber; i++) {
+            this._fish.push(new Fish(`f-${i}`, 50, 50, this._canvas.width, this._canvas.height));
+        }
+    }
+}
+const main = new Main();
+main.render();
