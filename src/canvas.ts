c0902e8f9af01c4b9956467dd87d65356776ddde
jiji-thecat <jijithekawaiicat@gmail.com>
2024-04-07 11:50:03 +0900

    add more fish, separate some parameters to fish class

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,44 +1,33 @@
-// const canvas = <HTMLCanvasElement>document.getElementById('canvas'); // cast
-// const ctx = <CanvasRenderingContext2D>canvas.getContext('2d');
-
-// const img = new Image();
-// let width = 0;
-// let height = 0;
-
-// function init() {
-//   img.src = '../assets/tuna.png';
-//   window.requestAnimationFrame(draw);
-// }
-
-// let i = 0;
-// function draw() {
-//   ctx.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
-//   ctx.globalCompositeOperation = 'source-over';
-//   ctx.fillStyle = 'skyblue';
-//   ctx.fillRect(0, 0, canvas.width, canvas.height);
-//   ctx.save();
-
-//   i++;
-//   ctx.drawImage(img, 0, 0, 100, 100);
-
-//   window.requestAnimationFrame(draw);
-// }
-
-// init();
 import Fish from './fish';
 
 // sin wave
-// sin波
 const fps = 30;
-let count = 0;
-let isUp = true;
-
-const assets = ['tuna', 'redfish', 'redfish'];
+const assets = [
+  'tuna',
+  'redfish',
+  'redfish',
+  'tuna',
+  'redfish',
+  'redfish',
+  'tuna',
+  'redfish',
+  'redfish',
+  'tuna',
+  'redfish',
+  'redfish',
+  'tuna',
+  'redfish',
+  'redfish',
+  'tuna',
+  'redfish',
+  'redfish',
+  'tuna',
+];
 const fish: Fish[] = [];
 
-const init2 = () => {
+const init = () => {
   assets.forEach((value) => {
-    fish.push(new Fish(value, 100, 100));
+    fish.push(new Fish(value, 50, 50));
   });
 
   animation();
@@ -47,72 +36,44 @@ const init2 = () => {
 const canvas = <HTMLCanvasElement>document.getElementById('canvas'); // cast
 const ctx = <CanvasRenderingContext2D>canvas.getContext('2d');
 ctx.globalCompositeOperation = 'destination-over';
-let c = 0;
+
 const animation = () => {
   setTimeout(() => {
     ctx.clearRect(0, 0, canvas.width, canvas.height); // clear canvas
 
     fish.forEach((f) => {
-      ctx.translate(0, 150);
+      const random = f.getRandomPosition();
+      ctx.translate(0, f.random);
 
-      if (isUp && f.x < canvas.width) {
-        const rad = (count * 10 * Math.PI) / 180;
-        f.x = rad * 10;
-        f.y = Math.sin(rad) * 10;
+      if (f.isRight && f.x < canvas.width) {
+        const rad = ((f.count * Math.PI) / 180) * random;
+        f.x = rad * random;
+        f.y = Math.sin(rad) * random;
 
         ctx.translate(f.x, f.y);
         ctx.scale(-1, 1);
-        count++;
+
+        f.addCount({ isTrue: true });
       } else if (f.x > 0) {
-        count--;
-        isUp = false;
-        const rad = (count * 10 * Math.PI) / 180;
-        f.x = rad * 10;
-        f.y = Math.sin(rad) * 10;
+        f.addCount({ isTrue: false });
+        f.isRight = false;
+
+        const rad = (f.count * random * Math.PI) / 180;
+        f.x = rad * random;
+        f.y = Math.sin(rad) * random;
+
         ctx.translate(f.x, f.y);
         ctx.scale(1, 1);
       } else {
-        isUp = true;
+        f.isRight = true;
       }
 
       ctx.drawImage(f.image, 0, 0, f.width, f.height);
       ctx.setTransform(1, 0, 0, 1, 0, 0);
     });
-    // const f = <Fish>fish.at(0);
-    // const f1 = <Fish>fish.at(1);
-    // const f2 = <Fish>fish.at(2);
 
-    // ctx.translate(c, 0);
-    // ctx.drawImage(f.image, 0, 0, f.width, f.height);
-
-    // ctx.setTransform(1, 0, 0, 1, 0, 0);
-
-    // ctx.translate(0, c);
-    // ctx.drawImage(f1.image, 0, 0, f1.width, f1.height);
-
-    // ctx.setTransform(1, 0, 0, 1, 0, 0);
-
-    // ctx.translate(c, c);
-    // ctx.drawImage(f2.image, 0, 0, f2.width, f2.height);
-
-    // c++;
-
-    // fish.forEach((f, index) => {
-    //   if (index === 0) {
-    //     ctx.translate(c, 0);
-    //   } else if (index === 1) {
-    //     ctx.translate(0, c);
-    //   } else if (index === 2) {
-    //     ctx.translate(c, c);
-    //   }
-
-    //   ctx.drawImage(f.image, 0, 0, f.width, f.height);
-
-    //   ctx.setTransform(1, 0, 0, 1, 0, 0);
-    // });
-    c++;
     requestAnimationFrame(animation);
   }, 1000 / fps);
 };
 
-init2();
+init();
--- src/fish.ts
+++ src/fish.ts
@@ -4,6 +4,9 @@ export default class Fish {
   private _height: number;
   private _x: number;
   private _y: number;
+  private _random: number;
+  private _isRight: boolean;
+  private _count: number;
 
   constructor(src: string, width: number, height: number) {
     this._image = new Image();
@@ -11,9 +14,11 @@ export default class Fish {
 
     this._width = width;
     this._height = height;
-
     this._x = 0;
     this._y = 0;
+    this._random = Math.random() * 200;
+    this._isRight = true;
+    this._count = 0;
   }
 
   get image(): HTMLImageElement {
@@ -36,6 +41,18 @@ export default class Fish {
     return this._y;
   }
 
+  get random(): number {
+    return this._random;
+  }
+
+  get isRight(): boolean {
+    return this._isRight;
+  }
+
+  get count(): number {
+    return this._count;
+  }
+
   set x(x: number) {
     this._x = x;
   }
@@ -43,4 +60,16 @@ export default class Fish {
   set y(y: number) {
     this._y = y;
   }
+
+  set isRight(isRight: boolean) {
+    this._isRight = isRight;
+  }
+
+  addCount = ({ isTrue }: { isTrue: boolean }) => {
+    this._count += isTrue ? 1 : -1;
+  };
+
+  getRandomPosition = () => {
+    return this._random % 20;
+  };
 }
